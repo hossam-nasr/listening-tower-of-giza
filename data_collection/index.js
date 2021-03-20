cc82ea20a1d0e375b469f4b1cb9982447b297f11
hossam-nasr <hossam.nasr99@gmail.com>
2021-03-20 02:45:29 -0400

    added ability to create directories in case of error

--- data_collection/index.js
+++ data_collection/index.js
@@ -1,11 +1,23 @@
 const sslkeylog = require("sslkeylog");
 const indices = require("./indices.json");
+const fs = require("fs");
+const { DATA_DIRECTORY } = require("./constants.js");
 const { domain_to_test_number, createSslKeyLogFile } = require("./helpers.js");
 const { launchDnsTest } = require("./dns.js");
 const { launchPageLoadTest } = require("./pageload.js");
 
 const domain = "madamasr.com";
-const policy = "us_control";
+const policy = "us_test";
+
+// Create directories if they don't exist
+const dir_path = `${DATA_DIRECTORY}/${policy}`;
+const screenshots_path = `${dir_path}/screenshots`;
+if (!fs.existsSync(dir_path)) {
+  fs.mkdirSync(dir_path);
+}
+if (!fs.existsSync(screenshots_path)) {
+  fs.mkdirSync(screenshots_path);
+}
 
 (async () => {
   try {
