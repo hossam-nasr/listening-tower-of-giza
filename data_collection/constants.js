e86d681606eefa240a7ef4f264368386b910d7da
hossam-nasr <hossam.nasr99@gmail.com>
2021-03-20 04:41:12 -0400

    added logging results in .csv

--- data_collection/constants.js
+++ data_collection/constants.js
@@ -1,1 +1,3 @@
 exports.DATA_DIRECTORY = "../data";
+exports.SCREENSHOTS_DIR_NAME = "screenshots";
+exports.DATA_CSV_FILE_NAME = "experiments.csv";
--- data_collection/pageload.js
+++ data_collection/pageload.js
@@ -4,26 +4,35 @@ const { startLogging } = require("./helpers.js");
 const { DATA_DIRECTORY } = require("./constants.js");
 
 const launchPageLoadTest = async (domain, number, policy, keylog_file) => {
+  let success = 1;
+  let status = "success";
+  const pid = await startLogging(
+    domain,
+    "pageload",
+    number,
+    policy,
+    keylog_file
+  );
   try {
     const url = `http://${domain}`;
-    const pid = await startLogging(
-      domain,
-      "pageload",
-      number,
-      policy,
-      keylog_file
-    );
+    console.log("Launching browser...");
     const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
+    console.log("Navigating to page...");
     await page.goto(url);
+    console.log("Capturing screenshot...");
     await page.screenshot({
       path: `${DATA_DIRECTORY}/${policy}/screenshots/screenshot_${domain}_${number}.png`,
     });
     await browser.close();
+    console.log("Success!");
     await new Promise((resolve) => setTimeout(resolve, 2000));
-    kill(pid);
   } catch (e) {
     console.log("Puppeteer error: ", e.message);
+    success = 0;
+    status = e.message;
   }
+  kill(pid);
+  return { success, status };
 };
 exports.launchPageLoadTest = launchPageLoadTest;
