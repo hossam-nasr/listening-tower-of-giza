532dc28460af69f7d3b81561a8b2a3c6c61f856d
Hossam Mabed <hossam.nasr99@gmail.com>
2021-03-26 18:33:43 +0200

    capture for nordvpn std nat

--- data_collection/index.js
+++ data_collection/index.js
@@ -17,7 +17,7 @@ const {
 const { launchDnsTest } = require("./dns.js");
 const { launchPageLoadTest } = require("./pageload.js");
 
-const policy = "egypt_nordvpn_obfs_bridged";
+const policy = "egypt_nordvpn_std_nat";
 
 (async () => {
   try {
